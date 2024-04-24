d9a436951a6a1284dee70d434964201691bc25bb
matsagad <matsagad@gmail.com>
2024-04-24 19:01:59 +0100

    Add constructor signature

--- protein/frames.py
+++ protein/frames.py
@@ -7,12 +7,17 @@ class Frames(ABC):
 
     @property
     @abstractmethod
-    def trans(self) -> Tensor:
-        """Translation vector"""
+    def rots(self) -> Tensor:
+        """Rotation matrix"""
         pass
 
     @property
     @abstractmethod
-    def rots(self) -> Tensor:
-        """Rotation matrix"""
+    def trans(self) -> Tensor:
+        """Translation vector"""
         pass
+
+    @abstractmethod
+    def __init__(self, rots: Tensor, trans: Tensor):
+        self.rots = rots
+        self.trans = trans
