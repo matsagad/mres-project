Fix incorrect grad x_t calculation for TDS; Add context managers to reuse cached model outputs

conditional/tds.py
@@ -17,8 +17,12 @@ class TDS(ConditionalWrapper):
     def with_config(
         self,
         resample_indices: Callable = residual_resample,
+        sigma: float = 0.05,
+        twist_scale: float = 1.0,
     ) -> "TDS":
         self.resample_indices = resample_indices
+        self.sigma = sigma
+        self.twist_scale = twist_scale
         return self
 
     def sample_given_motif(
@@ -30,84 +34,117 @@ class TDS(ConditionalWrapper):
         """
 
         N_TIMESTEPS = self.model.n_timesteps
-        K = mask.shape[0]
+        K, N_RESIDUES = mask.shape
+        OBSERVED_REGION = motif_mask[0] == 1
+        sigma = self.sigma
+        twist_scale = self.twist_scale
 
         # Set-up motif
         x_motif = self.model.coords_to_frames(motif, motif_mask)
 
-        # Initialise weights
+        def log_likelihood(x_zero_hat: Frames, t: Tensor) -> Tensor:
+            centred_x_zero_hat_trans = x_zero_hat.trans[
+                :, OBSERVED_REGION
+            ] - torch.mean(x_zero_hat.trans[:, OBSERVED_REGION], dim=1).unsqueeze(1)
+            return -0.5 * (
+                ((centred_x_zero_hat_trans - x_motif.trans[:, OBSERVED_REGION]) ** 2)
+                / (self.model.forward_variance[t].view(-1, 1, 1) + sigma**2)
+            ).sum(dim=(1, 2))
+
+        # Sample frames
         T = torch.tensor([N_TIMESTEPS - 1] * K, device=self.device).long()
         x_T = self.model.sample_frames(mask)
-        log_p_tilde_T = self.optimal_twisting_log_likelihood(
-            x_motif, x_T, T, motif_mask, mask
-        )
-        log_p_tilde_T -= torch.logsumexp(log_p_tilde_T, dim=0)
+        x_T.trans.requires_grad = True
 
-        w = torch.exp(log_p_tilde_T)
-        w /= torch.sum(w)
+        # Initialise weights
+        score = self.model.score(x_T, T, mask)
+        ## Custom context manager for caching score to avoid repeated calls to model
+        with self.model.with_score(score):
+            x_zero_hat = self.model.predict_fully_denoised(x_T, T, mask)
 
-        ess = (1 / torch.sum(w**2)).cpu()
+        ## Calculate log likelihood and its gradient
+        log_p_tilde_T = log_likelihood(x_zero_hat, T)
+        log_p_tilde_T = log_p_tilde_T - torch.logsumexp(log_p_tilde_T, dim=0)
+        grad_log_p_tilde_T = torch.autograd.grad(log_p_tilde_T.sum(), x_T.trans)[0]
+
+        x_T.trans = x_T.trans.detach()
+
+        with torch.no_grad():
+            w = torch.exp(log_p_tilde_T)
+            w /= torch.sum(w)
+            ess = (1 / torch.sum(w**2)).cpu()
 
         x_t = x_T
-        log_p_tilde_t = log_p_tilde_T
+        log_p_tilde_t = log_p_tilde_T.detach()
+        grad_log_p_tilde_t = grad_log_p_tilde_T.detach()
 
         x_trajectory = [x_T]
         pf_stats = {"ess": [], "w": []}
 
         with torch.no_grad():
             for i in tqdm(
-                reversed(range(N_TIMESTEPS)),
+                reversed(range(N_TIMESTEPS - 1)),
                 desc="Reverse diffusing samples",
                 total=N_TIMESTEPS,
                 disable=not self.verbose,
             ):
                 # Resample particles
-                if ess < 0.5 * K:
+                if ess <= 0.5 * K:
                     resampled_indices = self.resample_indices(w)
                     x_t.rots = x_t.rots[resampled_indices]
                     x_t.trans = x_t.trans[resampled_indices]
                     log_p_tilde_t = log_p_tilde_t[resampled_indices]
+                    w[:] = 1 / K
+
+                ## Recenter with respect to motif segment's center-of-mass
+                ## (This is not necessary but makes for easier visual comparison when plotted)
+                x_t.trans[:, :N_RESIDUES] -= torch.mean(
+                    x_t.trans[:, OBSERVED_REGION], dim=1
+                ).unsqueeze(1)
 
                 t = torch.tensor([i] * K, device=self.device).long()
 
-                # Conditional score approximation s_{\theta}(x^{t + 1}, y=x_{M}^{0})
-                ## [B, N_AA, 3]  +  [B, 1, 1]
-                cond_score = self.model.score(
-                    x_t, t, mask
-                ) + self.optimal_twisting_grad_log_likelihood(
-                    x_motif, x_t, t, motif_mask, mask
-                ).view(
-                    -1, 1, 1
-                )
-
-                # Propose next step
                 x_t_plus_one = x_t
                 t_plus_one = t
 
-                x_t = self.reverse_diffuse_with_conditional_score(
-                    x_t_plus_one, t_plus_one, mask, cond_score
-                )
-                t = t_plus_one - 1
+                # Propose next step
+                ## Conditional score approximation s_{\theta}(x^{t + 1}, y=x_{M}^{0})
+                cond_score = score + grad_log_p_tilde_t * twist_scale
+                with self.model.with_score(cond_score):
+                    x_t = self.model.reverse_diffuse(x_t_plus_one, t_plus_one, mask)
+
                 x_trajectory.append(x_t)
 
-                # Update weights
-                ## Note: we normalise each component here to avoid (possibly rare?) overflows
-                ## but this shouldn't change the final quantity
+                t = t_plus_one - 1
                 log_p_tilde_t_plus_one = log_p_tilde_t
 
-                log_p_tilde_t = self.optimal_twisting_log_likelihood(
-                    x_motif, x_t, t, motif_mask, mask
-                )
-                log_p_tilde_t -= torch.logsumexp(log_p_tilde_t, dim=0)
+                # Update weights
+                x_t.trans.requires_grad = True
+                with torch.enable_grad():
+                    score = self.model.score(x_t, t, mask)
+                    with self.model.with_score(score):
+                        x_zero_hat = self.model.predict_fully_denoised(x_t, t, mask)
 
-                reverse_llik = self.model.reverse_log_likelihood(
-                    x_t, x_t_plus_one, t_plus_one, mask, mask
-                )
+                    log_p_tilde_t = log_likelihood(x_zero_hat, t)
+                    log_p_tilde_t = log_p_tilde_t - torch.logsumexp(
+                        log_p_tilde_t, dim=0
+                    )
+                    grad_log_p_tilde_t = torch.autograd.grad(
+                        log_p_tilde_t.sum(), x_t.trans
+                    )[0]
+                x_t.trans = x_t.trans.detach()
+
+                ## Reverse log likelihoods
+                with self.model.with_score(score):
+                    reverse_llik = self.model.reverse_log_likelihood(
+                        x_t, x_t_plus_one, t_plus_one, mask, mask
+                    )
                 reverse_llik -= torch.logsumexp(reverse_llik, dim=0)
 
-                reverse_cond_llik = self.reverse_log_likelihood_with_conditional_score(
-                    x_t, x_t_plus_one, t_plus_one, mask, cond_score
-                )
+                with self.model.with_score(cond_score):
+                    reverse_cond_llik = self.model.reverse_log_likelihood(
+                        x_t, x_t_plus_one, t_plus_one, mask, mask
+                    )
                 reverse_cond_llik -= torch.logsumexp(reverse_cond_llik, dim=0)
 
                 log_w = (reverse_llik + log_p_tilde_t) - (
@@ -115,10 +152,14 @@ class TDS(ConditionalWrapper):
                 )
                 log_w = log_w - torch.max(log_w)
                 log_sum_w = torch.logsumexp(log_w, dim=0)
-                w = torch.exp(log_w - log_sum_w)
-                w /= torch.sum(w)
 
-                ess = (1 / torch.sum(w**2)).cpu()
+                w *= torch.exp(log_w - log_sum_w)
+                if torch.all(w == 0):
+                    w[:] = 1 / K
+                    ess = 0
+                else:
+                    w /= w.sum()
+                    ess = (1 / torch.sum(w**2)).cpu()
 
                 pf_stats["w"].append(w.cpu())
                 pf_stats["ess"].append(ess)
@@ -126,90 +167,3 @@ class TDS(ConditionalWrapper):
         self.save_stats(pf_stats)
 
         return x_trajectory
-
-    def optimal_twisting_log_likelihood(
-        self, x_motif: Frames, x_t: Frames, t: Tensor, motif_mask: Tensor, mask: Tensor
-    ) -> Tensor:
-        # \log(\tilde{p}_{\theta}(y | x^{t}, M))
-        MOTIF_SEGMENT = motif_mask[0] == 1
-
-        # TODO: Is it correct to center the motif region at zero? Otherwise predicted
-        # \hat{x}^{0} can be magnitudes away.
-        mu = self.model.predict_fully_denoised(x_t, t, mask).trans[:, MOTIF_SEGMENT]
-        mu = mu - torch.mean(mu, dim=1, keepdim=True)
-        sigma_squared = self.model.forward_variance[t]
-
-        log_density = (
-            -0.5
-            * ((x_motif.trans[:, MOTIF_SEGMENT] - mu) ** 2)
-            / sigma_squared.view(-1, 1, 1)
-        ).sum(
-            axis=(1, 2)
-        )  # - 0.5 * torch.log(2 * torch.pi * sigma_squared)
-
-        return log_density
-
-    def optimal_twisting_grad_log_likelihood(
-        self, x_motif: Frames, x_t: Frames, t: Tensor, motif_mask: Tensor, mask: Tensor
-    ) -> Tensor:
-        # \nabla_{x^{t}}\log(\tilde{p}_{\theta}(y | x^{t}, M))
-        MOTIF_SEGMENT = motif_mask[0] == 1
-
-        mu = self.model.predict_fully_denoised(x_t, t, mask).trans[:, MOTIF_SEGMENT]
-        mu = mu - torch.mean(mu, dim=1, keepdim=True)
-        sigma_squared = self.model.forward_variance[t].view(-1, 1, 1)
-
-        grad_log_density = (
-            -(x_motif.trans[:, MOTIF_SEGMENT] - mu) / sigma_squared
-        ).sum(axis=(1, 2))
-
-        return grad_log_density
-
-    def reverse_diffuse_with_conditional_score(
-        self,
-        x_t: Frames,
-        t: Tensor,
-        mask: Tensor,
-        score: Tensor,
-        noise_scale: float = 1,
-    ) -> Frames:
-        # \sim \tilde{p}_{\theta}(x^{t - 1} | x^{t}, y)
-        mu = (x_t.trans + self.model.variance[t].view(-1, 1, 1) * score) / torch.sqrt(
-            1 - self.model.variance[t].view(-1, 1, 1)
-        )
-        sigma_tilde = self.model.sqrt_variance[t].view(-1, 1, 1)
-
-        sample_trans = mu + noise_scale * sigma_tilde * torch.randn(
-            x_t.trans.shape, device=self.device
-        )
-        sample_trans *= mask[..., None]
-        x_t_minus_one = self.model.coords_to_frames(sample_trans, mask)
-
-        return x_t_minus_one
-
-    def reverse_log_likelihood_with_conditional_score(
-        self,
-        x_t_minus_one: Frames,
-        x_t: Frames,
-        t: Tensor,
-        mask: Tensor,
-        score: Tensor,
-    ) -> Tensor:
-        # \log(\tilde{p}_{\theta}(x^{t - 1} | x^{t}, y))
-        # We assume mask is the same for all the particles.
-        MASK_SEGMENT = mask[0] == 1
-
-        mu = (
-            x_t.trans[:, MASK_SEGMENT] + self.model.variance[t].view(-1, 1, 1) * score
-        ) / torch.sqrt(1 - self.model.variance[t].view(-1, 1, 1))
-        sigma_tilde_squared = self.model.variance[t]
-
-        log_density = (
-            -0.5
-            * ((x_t_minus_one.trans[:, MASK_SEGMENT] - mu) ** 2)
-            / sigma_tilde_squared.view(-1, 1, 1)
-        ).sum(
-            axis=(1, 2)
-        )  # - 0.5 * torch.log(2 * torch.pi * sigma_tilde_squared)
-
-        return log_density

main.py
@@ -106,6 +106,8 @@ def sample_given_motif(cfg: DictConfig) -> None:
     elif method == "tds":
         setup = TDS(model).with_config(
             resample_indices=resampling_method,
+            sigma=cond_cfg.sigma,
+            twist_scale=cond_cfg.twist_scale,
         )
 
     elif method == "fpssmc":
@@ -145,7 +147,7 @@ def sample_given_motif(cfg: DictConfig) -> None:
         os.makedirs(os.path.join(out, "traces"))
         # [K, T, N_AA, 3]
         samples_trans = torch.stack(
-            [sample.trans[mask[0] == 1].detach().cpu() for sample in samples]
+            [sample.trans[:, mask[0] == 1].detach().cpu() for sample in samples]
         ).swapaxes(0, 1)
 
         for i, sample_trace in enumerate(samples_trans):

model/genie.py
@@ -25,6 +25,9 @@ class GenieAdapter(FrameDiffusionModel):
         self.forward_variance = self.model.one_minus_alphas_cumprod
         self.sqrt_forward_variance = self.model.sqrt_one_minus_alphas_cumprod
 
+        self._cached_epsilon = None
+        self._cached_score = None
+
     def from_weights_and_config(f_weights: str, f_config: str) -> "GenieAdapter":
         config = Config(f_config)
         model = Genie.load_from_checkpoint(f_weights, config=config)
@@ -102,9 +105,19 @@ class GenieAdapter(FrameDiffusionModel):
         return log_density
 
     def reverse_diffuse(
-        self, x_t: Frames, t: Tensor, mask: Tensor, noise_scale: float
+        self, x_t: Frames, t: Tensor, mask: Tensor, noise_scale: float = 1.0
     ) -> Frames:
-        return self.model.p(x_t, t, mask, noise_scale)
+        x_t_minus_one_trans = (
+            x_t.trans + self.model.betas[t].view(-1, 1, 1) * self.score(x_t, t, mask)
+        ) / self.model.sqrt_alphas[t].view(-1, 1, 1)
+
+        x_t_minus_one_trans += (
+            noise_scale
+            * self.model.sqrt_betas[t].view(-1, 1, 1)
+            * torch.randn(x_t_minus_one_trans.shape, device=self.device)
+        )
+        x_t_minus_one = self.coords_to_frames(x_t_minus_one_trans, mask)
+        return x_t_minus_one
 
     def reverse_diffuse_deterministic(
         self, x_t: Frames, t: Tensor, mask: Tensor
@@ -117,6 +130,20 @@ class GenieAdapter(FrameDiffusionModel):
         return x_t_minus_one
 
     def _epsilon(self, x_t: Frames, t: Tensor, mask: Tensor) -> Tensor:
+        if self._cached_epsilon is not None:
+            assert (
+                x_t.trans.shape == self._cached_epsilon.shape
+            ), "Mismatch between cached epsilon and input frame."
+            return self._cached_epsilon
+
+        if self._cached_score is not None:
+            assert (
+                x_t.trans.shape == self._cached_score.shape
+            ), "Mismatch between cached score and input frame."
+            return -self._cached_score * self.model.sqrt_one_minus_alphas_cumprod[
+                t
+            ].view(-1, 1, 1)
+
         denoised_pile = []
         for batch in torch.split(torch.arange(x_t.shape[0]), self.batch_size):
             curr_batch_size = len(batch)