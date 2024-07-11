4207322090b0c05a7b1dfebc6d52e56cf954a8ef
matsagad <matsagad@gmail.com>
2024-07-11 20:21:26 +0100

    Set HYDRA_FULL_ERROR variable to override hydra error handling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from experiments import EXPERIMENTS_REGISTRY, placeholder_job
 import hydra
 import logging
 from omegaconf import DictConfig, OmegaConf
+import os
 import sys
 import traceback
 from types import TracebackType
@@ -21,13 +22,18 @@ def main(cfg: DictConfig) -> None:
     EXPERIMENTS_REGISTRY.get(experiment_name, placeholder_job)(cfg)
 
 
-def log_exception(_type: type, value: BaseException, tb: TracebackType) -> None:
-    if issubclass(_type, KeyboardInterrupt):
-        sys.__excepthook__(_type, value, tb)
+def log_exception(
+    exc_type: type, exc_value: BaseException, exc_traceback: TracebackType
+) -> None:
+    exc_info = (exc_type, exc_value, exc_traceback)
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(*exc_info)
         return
-    logger.exception("".join(traceback.format_exception(_type, value, tb)))
+    logger.error("".join(traceback.format_exception(*exc_info)))
 
 
+# Need to set environment variable to enable override of sys.excepthook
+os.environ["HYDRA_FULL_ERROR"] = "1"
 sys.excepthook = log_exception
 
 if __name__ == "__main__":
